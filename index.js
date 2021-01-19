6366c5f9398804445e3c61f2ba8de4e1fd051f31
Ben Hardill <hardillb@gmail.com>
2021-01-19 22:25:40 +0000

    Fix case on traefik hostname label

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ app.post('/instance', passport.authenticate(['basic'],{session: true}), function
 				    NetworkMode: "internal"
 				  }
 				};
-				contOptions.Labels["traefik.http.routers." + appname + ".rule" ] = "HOST(`" + hostname + "`)"
+				contOptions.Labels["traefik.http.routers." + appname + ".rule" ] = "Host(`" + hostname + "`)"
 				contOptions.Labels["traefik.http.services." + appname + ".loadbalancer.server.port"] = "1880"
 				contOptions.Labels["traefik.http.routers." + appname + ".entrypoints"] = "web"
 				return docker.createContainer(contOptions)
